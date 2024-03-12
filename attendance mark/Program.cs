de1ebaeb14fd2efaa7bea4c6c2e1b359fc308b02
Slava <polsha13esma@mail.ru>
2024-03-12 11:50:43 +0300

    Добавьте файлы проекта.

---
+++ attendance mark/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace attendance_overview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Олег", "РПМ");
+            student.Visit();
+
+
+
+
+
+        }
+    }
+    class Student
+    {
+        private string _name;
+
+        public string AcademicSubjectName;
+
+        public Student(string name, string academicSubjectName)
+        {
+            _name = name;
+            AcademicSubjectName = academicSubjectName;
+        }
+
+        public void Visit()
+        {
+            while (true)
+            //Запрашиваем у пользователя название предмета который сейчас преподают 
+
+            {
+                Console.WriteLine($"\nЗдравствуйте {_name}\nВведите название  предмета  ");
+                string inputNameSubject = Convert.ToString(Console.ReadLine());
+                //Сравниваем название указаного предмета с предметом из списка 
+                if (inputNameSubject.ToLower() != AcademicSubjectName.ToLower())
+                {
+                    Console.WriteLine("Такого предмета не существует!\nНажмите любую клавишу что бы начать заного");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    //После проверки предмета переходим к выбору команды находился ли студент на паре и так же проверяем правильность ввода
+
+                    Console.WriteLine("\nВыберите номер команды:\n1 - присутствовал на паре\n2 - отсутствовал");
+                    int visit = Convert.ToInt32(Console.ReadLine());
+                    switch (visit)
+                    {
+                        case 1:
+                            Console.WriteLine("Вы молодец! Хорошей вас пары");
+                            break;
+                        case 2:
+                            Console.WriteLine("Жаль, вам необходимо придоставить справку о причине отсутствия ");
+                            break;
+                        default:
+                            Console.WriteLine("Вы выбрали неверную команду. Нажмите любую клавишу что бы начать сначала!");
+                            break;
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    // выводим общую информацию о посещаемости студента  за месяц на данный момент 
+                    Console.WriteLine("Ваша посещаемость на данный момент:");
+                    {
+                        Random rand = new Random();
+                        int LessonPerMonth = rand.Next(10, 35);
+                        int LessonAttended = rand.Next(10, LessonPerMonth);
+                        double PrecentAttendance = (double)LessonAttended / LessonPerMonth * 100;
+                        int RoundedPercent = (int)Math.Round(PrecentAttendance, 0);
+
+                        Console.WriteLine($"В этом месяце вы посетили: {RoundedPercent}% процента всех занятий!");
+
+                        if (PrecentAttendance >= 50f)
+                            Console.WriteLine("Хороший результат, так держать!");
+
+                        else
+                            Console.WriteLine("Плохо,вам стоит чаще посещать занятия ");
+
+                    }
+
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+
+    }
+}
+
---
+++ attendance mark/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("attendance mark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("attendance mark")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("afcf812e-34a0-4362-9285-a5a474453675")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
